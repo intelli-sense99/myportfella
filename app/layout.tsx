fa2881338eb176c746dfdce7d17e691528399570
vikaskumar36 <vikaskumar@klynetech.com>
2026-01-08 12:38:25 +0530

    skilles design changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className="dark">
       <body>
         <WaterWave />
-        <CursorEffect />
+        {/* <CursorEffect /> */}
         <ConditionalLayout>
           {children}
         </ConditionalLayout>
